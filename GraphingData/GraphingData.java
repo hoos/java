492f8bf2c05915a0bb09ff505c08230fa8d2cc92
hoos <husseinb01@gmail.com>
2017-09-19 00:37:04 +0100

    Moved functionality into methods

--- GraphingData/GraphingData.java
+++ GraphingData/GraphingData.java
@@ -7,14 +7,6 @@ import java.util.*;
 
 public class GraphingData extends JPanel {
 
-    public int[] getData() {
-        int[] data = new int[100000];
-        for (int i = 0; i < data.length; i++) {
-            data[i] = GraphingData.getRandInt(0,99999999);
-        };
-        return data;
-    }
-
     final int LMARGIN = 30;
     final int RMARGIN = 10;
     final int TMARGIN = 10;
@@ -24,27 +16,11 @@ public class GraphingData extends JPanel {
     float [] xdata = new float[199];
     float [] ydata = new float[199];
 
-    protected void paintComponent(Graphics g) {
-        Scanner sc = new Scanner(System.in); 
-        int idx = 0;
-        while (sc.hasNextFloat()) {
-            xdata[idx] = sc.nextFloat();
-            ydata[idx] = sc.nextFloat();
-            idx++;
-        }
-        //System.out.println(Arrays.toString(xdata));
-        //System.out.println(Arrays.toString(ydata));
-        int [] data = getData();
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                            RenderingHints.VALUE_ANTIALIAS_ON);
+    protected void drawOrdinate(Graphics2D g2) {
         int w = getWidth();
         int h = getHeight();
-        System.out.println("width=" + w + " height " + h);
         Font font = g2.getFont();
         FontRenderContext frc = g2.getFontRenderContext();
-        // Draw ordinate.
         g2.draw(new Line2D.Double(LMARGIN, TMARGIN, LMARGIN, h-BMARGIN));
         int tick_label = 0;
         for (int i = h - BMARGIN; i >= TMARGIN; i-=(h/30)) {
@@ -57,7 +33,13 @@ public class GraphingData extends JPanel {
             g2.drawString(letter, sx, sy);
             tick_label += 10;
         } 
-        // Draw abcissa.
+    }
+
+    protected void drawAbcissa(Graphics2D g2) {
+        int w = getWidth();
+        int h = getHeight();
+        Font font = g2.getFont();
+        FontRenderContext frc = g2.getFontRenderContext();
         g2.draw(new Line2D.Double(LMARGIN, h-LMARGIN, w-RMARGIN, h-LMARGIN));
         int step = 0;
         float x_tick_label = xdata[step];
@@ -73,8 +55,28 @@ public class GraphingData extends JPanel {
             step += 2;
             x_tick_label = xdata[step];
         }
-        // Draw Labels
-        // Ordinate label.
+
+    }
+
+    protected void drawLine(Graphics2D g2) {
+        int w = getWidth();
+        int h = getHeight();
+        g2.setPaint(Color.blue);
+        for(int i = 0; i < 198; i++) {
+            double x1 = LMARGIN + (i * 10)  ;
+            double y1 = h - BMARGIN - (ydata[i] * h/300);
+            double x2 = LMARGIN + (i + 1) * 10 ;
+            double y2 = h - BMARGIN - (ydata[i+1] * h/300);
+            //System.out.println("x1=" + x1 + " y1=" + y1 + " x2=" +  x2  + " y2=" + y2);
+            g2.draw(new Line2D.Double(x1, y1, x2, y2));
+        }
+    }
+
+    protected void drawOrdinateLabel(Graphics2D g2) {
+        int w = getWidth();
+        int h = getHeight();
+        Font font = g2.getFont();
+        FontRenderContext frc = g2.getFontRenderContext();
         String s = "y axis";
         LineMetrics lm = font.getLineMetrics("0", frc);
         float sh = lm.getAscent() + lm.getDescent();
@@ -87,25 +89,26 @@ public class GraphingData extends JPanel {
             g2.drawString(letter, sx, sy);
             sy += sh;
         }
-        // Abcissa label.
-        s = "x axis";
-        sy = h - PAD + (PAD - sh)/2 + lm.getAscent();
+    }
+
+    protected void drawAbcissaLabel(Graphics2D g2) {
+        int w = getWidth();
+        int h = getHeight();
+        Font font = g2.getFont();
+        FontRenderContext frc = g2.getFontRenderContext();
+        String s = "x axis";
+        LineMetrics lm = font.getLineMetrics("0", frc);
+        float sh = lm.getAscent() + lm.getDescent();
+        float sy = h - PAD + (PAD - sh)/2 + lm.getAscent();
         float sw = (float)font.getStringBounds(s, frc).getWidth();
         float sx = (w - sw)/2;
         g2.setFont(new Font("SanSerif", Font.PLAIN, 12));
         g2.drawString(s, sx, sy);
-        // Draw Line
-        g2.setPaint(Color.blue);
-        for(int i = 0; i < 198; i++) {
-            double x1 = LMARGIN + (i * 10)  ;
-            double y1 = h - BMARGIN - (ydata[i] * h/300);
-            double x2 = LMARGIN + (i + 1) * 10 ;
-            double y2 = h - BMARGIN - (ydata[i+1] * h/300);
-            
-            //System.out.println("x1=" + x1 + " y1=" + y1 + " x2=" +  x2  + " y2=" + y2);
-            g2.draw(new Line2D.Double(x1, y1, x2, y2));
-        }
-        // Mark data points.
+    }
+
+    protected void drawDataPoints(Graphics2D g2) {
+        int w = getWidth();
+        int h = getHeight();
         g2.setPaint(Color.red);
         for(int i = 0; i < 199; i++) {
             double x = LMARGIN + i * 10;
@@ -115,27 +118,48 @@ public class GraphingData extends JPanel {
         }
     }
 
-    private int getMax() {
-        int[] data = getData();
-        int max = -Integer.MAX_VALUE;
-        for(int i = 0; i < data.length; i++) {
-            if(data[i] > max)
-                max = data[i];
+    protected void paintComponent(Graphics g) {
+        Scanner sc = new Scanner(System.in); 
+        int idx = 0;
+        while (sc.hasNextFloat()) {
+            xdata[idx] = sc.nextFloat();
+            ydata[idx] = sc.nextFloat();
+            idx++;
         }
-        return max;
+        //System.out.println(Arrays.toString(xdata));
+        //System.out.println(Arrays.toString(ydata));
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                            RenderingHints.VALUE_ANTIALIAS_ON);
+        int w = getWidth();
+        int h = getHeight();
+        System.out.println("width=" + w + " height " + h);
+        Font font = g2.getFont();
+        FontRenderContext frc = g2.getFontRenderContext();
+        // Draw ordinate.
+        drawOrdinate(g2);
+        // Draw abcissa.
+        drawAbcissa(g2);
+           // Draw Labels
+        // Ordinate label.
+        drawOrdinateLabel(g2);
+        // Abcissa label.
+        drawAbcissaLabel(g2);
+        // Draw Line
+        drawLine(g2);
+        // Draw data points.
+        drawDataPoints(g2);
     }
 
     public static void main(String[] args) {
         JFrame f = new JFrame();
+        f.setTitle("Total Sun Spot Number From 1818: Source SILSO");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.add(new GraphingData());
         f.setExtendedState( f.getExtendedState()|JFrame.MAXIMIZED_BOTH );
         f.setSize(2048,1152);
         f.setLocation(0,0);
         f.setVisible(true);
-    }
-
-    public static int getRandInt(int min, int max) {
-        return (int) (Math.random() * max + min);
     }
 }
